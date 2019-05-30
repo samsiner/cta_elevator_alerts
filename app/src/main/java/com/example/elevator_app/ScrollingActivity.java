531c1421a8ca64cd86859d75efb7e2980b7329ba
ssiner <ssiner@luc.edu>
2019-05-29 20:38:48 -0500

    create project

---
+++ app/src/main/java/com/example/elevator_app/ScrollingActivity.java
@@ -0,0 +1,61 @@
+package com.example.elevator_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    private TextView stationsTempOut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        stationsTempOut = findViewById(R.id.text_elevDownTempList);
+        pullAlerts();
+    }
+
+    private void pullAlerts(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                StringBuilder builder = new StringBuilder();
+                builder.append("Stations Temporarily Out\n");
+                stationsTempOut.setText(builder.toString());
+            }
+        });
+
+
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
