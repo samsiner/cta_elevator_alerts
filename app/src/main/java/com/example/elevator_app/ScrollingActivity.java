7c9a00416f70a70bfab9ea8ea3fc7731e14819a5
Tyler Arndt <tsarndt16@gmail.com>
2019-06-09 17:47:54 -0500

    added Tyler comment

--- app/src/main/java/com/example/elevator_app/ScrollingActivity.java
+++ app/src/main/java/com/example/elevator_app/ScrollingActivity.java
@@ -23,7 +23,7 @@ import javax.xml.parsers.*;
 import android.widget.LinearLayout.LayoutParams;
 
 public class ScrollingActivity extends AppCompatActivity {
-    //Tylers comment
+    //Tyler's comment
     //Sam's comment
     private TextView stationsTempOut;
     private LinearLayout linearLayout;
