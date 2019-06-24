d360f4249a9fe37c2c81fc02742f146b420228a2
Tyler Arndt <tsarndt16@gmail.com>
2019-06-24 16:03:41 -0500

    Added line colors to colors.xml

--- app/src/main/java/com/example/elevator_app/MainActivity.java
+++ app/src/main/java/com/example/elevator_app/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scrolling);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         stationsTempOut = findViewById(R.id.text_elevDownTempList);
