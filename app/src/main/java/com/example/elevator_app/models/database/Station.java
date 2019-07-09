f7fbb1f1aed1758873e804af9faf793bcc7a8243
ssiner <ssiner@luc.edu>
2019-07-09 10:40:15 -0500

    Create adapter

--- app/src/main/java/com/example/elevator_app/models/database/Station.java
+++ app/src/main/java/com/example/elevator_app/models/database/Station.java
@@ -4,14 +4,63 @@ import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 @Entity(tableName = "station_table")
 public class Station {
 
     @PrimaryKey
     @NonNull
-    public String stationID;
-    public boolean hasElevator;
-    public String[] routes;
-    public boolean hasElevatorAlert;
-    public String headline, shortDescription, beginDateTime;
+    private String stationID;
+    private boolean hasElevator, hasElevatorAlert;
+    private boolean[] routes;
+    private String name, headline, shortDescription, beginDateTime;
+
+    public Station(String stationID, String name, boolean hasElevator, boolean[] routes){
+        this.stationID = stationID;
+        this.name = name;
+        this.hasElevator = hasElevator;
+        this.routes = routes;
+        hasElevatorAlert = false;
+        headline = "";
+        shortDescription = "";
+        beginDateTime = "";
+    }
+
+    public String getStationID(){ return stationID; }
+    public String getName(){ return name; }
+    public String getHeadline(){ return headline; }
+    public String getShortDescription(){ return shortDescription; }
+    public String getBeginDateTime(){ return beginDateTime; }
+    public boolean isHasElevator(){ return hasElevator; }
+    public boolean isHasElevatorAlert(){ return hasElevatorAlert; }
+    public boolean[] getRoutes(){ return routes; }
+
+    public void addAlert(String headline, String shortDescription, String beginDateTime){
+        this.headline = headline;
+        this.shortDescription = shortDescription;
+        this.beginDateTime = convertDateTime(beginDateTime);
+        hasElevatorAlert = true;
+    }
+
+    public void removeAlert(){
+        this.headline = "";
+        this.shortDescription = "";
+        this.beginDateTime = "";
+        hasElevatorAlert = false;
+    }
+
+    private String convertDateTime(String s){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy'-'MM'-'dd'T'HH:mm:ss", Locale.US);
+        try {
+            Date originalDate = dateFormat.parse(s);
+            SimpleDateFormat dateFormat2 = new SimpleDateFormat("MMMM' 'dd', 'yyyy' at 'h:mm a", Locale.US);
+            return dateFormat2.format(originalDate);
+        } catch (ParseException e) {
+            return s;
+        }
+    }
 }
---
+++ app/src/main/java/com/example/elevator_app/models/database/StationAlertsAdapter.java
@@ -0,0 +1,67 @@
+package com.example.elevator_app.models.database;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.elevator_app.R;
+
+import java.util.List;
+
+public class StationAlertsAdapter extends RecyclerView.Adapter<StationAlertsAdapter.StationAlertViewHolder> {
+
+    class StationAlertViewHolder extends RecyclerView.ViewHolder {
+        private final TextView stationAlertTextView;
+        private final ImageView stationAlertImageView;
+
+        private StationAlertViewHolder(View itemView) {
+            super(itemView);
+            stationAlertTextView = itemView.findViewById(R.id.txt_alert_station);
+            stationAlertImageView = itemView.findViewById(R.id.img_alert_station);
+        }
+    }
+
+    private final LayoutInflater mInflater;
+    private List<Station> mStations;
+
+    StationAlertsAdapter(Context context){ mInflater = LayoutInflater.from(context);}
+
+    @Override
+    public StationAlertViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View itemView = mInflater.inflate(R.layout.alert_station, parent, false);
+        return new StationAlertViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(StationAlertViewHolder holder, int position){
+        if (mStations != null){
+            Station current = mStations.get(position);
+            holder.stationAlertImageView.setImageResource(R.drawable.status_green);
+            holder.stationAlertTextView.setText(current.getName());
+
+//            holder.st.setOnClickListener(v -> {
+//                Intent intent = new Intent(MainActivity.this, DisplayAlertActivity.class);
+//                intent.putExtra("Station", s);
+//                startActivity(intent);
+//            });
+        }
+    }
+
+    void setStations(List<Station> stations){
+        mStations = stations;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount(){
+        if (mStations != null) return mStations.size();
+        else return 0;
+    }
+}
+
+
