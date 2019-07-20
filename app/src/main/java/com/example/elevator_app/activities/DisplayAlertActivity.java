6c5666a1a4092837519772e2c0efdc1783ce6c97
Tyler Arndt <tsarndt16@gmail.com>
2019-07-20 09:40:39 -0500

    Update UI for Display Alert Activity

--- app/src/main/java/com/example/elevator_app/activities/DisplayAlertActivity.java
+++ app/src/main/java/com/example/elevator_app/activities/DisplayAlertActivity.java
@@ -1,12 +1,15 @@
 package com.example.elevator_app.activities;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.elevator_app.R;
@@ -19,35 +22,50 @@ public class DisplayAlertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_alert);
+        TextView tv_beginDateTime = findViewById(R.id.txt_alert_date_time);
+//        TextView tv_headline = findViewById(R.id.txt_alert_headline);
+        TextView tv_shortDesc = findViewById(R.id.txt_alert_shortDesc);
         TextView toolbarText = findViewById(R.id.txt_toolbar);
 
         String stationID = getIntent().getStringExtra("stationID");
 
         //Get ViewModel
         DisplayAlertViewModel mDisplayAlertViewModel = ViewModelProviders.of(this, new DisplayAlertViewModelFactory(this.getApplication(), stationID)).get(DisplayAlertViewModel.class);
-        String headline = mDisplayAlertViewModel.getHeadline();
+//        String headline = mDisplayAlertViewModel.getHeadline();
         String shortDesc = mDisplayAlertViewModel.getShortDesc();
         String beginDateTime = mDisplayAlertViewModel.getBeginDateTime();
         String stationName = mDisplayAlertViewModel.getStationName();
 
         toolbarText.setText(stationName);
-        TextView display = findViewById(R.id.txt_display_alert);
-        display.setTextSize(15);
-        display.setTextColor(Color.BLACK);
-        display.append(stationName + "\n\n");
-        if (!beginDateTime.equals("")){
-            display.append("Starting ");
-            display.append(beginDateTime + "\n\n");
-            display.append(headline + "\n\n");
-            display.append(shortDesc + "\n\n");
+        if(!beginDateTime.equals("")){
+            tv_beginDateTime.setText(beginDateTime);
+//            tv_headline.setText(headline);
+            tv_shortDesc.setText(shortDesc);
         } else{
+            tv_beginDateTime.setBackgroundResource(0);
+            tv_beginDateTime.setTypeface(null, Typeface.NORMAL);
             boolean hasElevator = mDisplayAlertViewModel.getHasElevator();
-            if (hasElevator) {
-                display.append("Elevator is present at this station and working properly.");
+            if(hasElevator){
+                tv_beginDateTime.setText(R.string.present_elevator);
             } else{
-                display.append("No elevator is present at this station.");
+                tv_beginDateTime.setText(R.string.no_elevator);
             }
         }
+//        TextView display = findViewById(R.id.txt_display_alert);
+//        display.append(stationName + "\n\n");
+//        if (!beginDateTime.equals("")){
+//            display.append("Starting ");
+//            display.append(beginDateTime + "\n\n");
+//            display.append(headline + "\n\n");
+//            display.append(shortDesc + "\n\n");
+//        } else{
+//            boolean hasElevator = mDisplayAlertViewModel.getHasElevator();
+//            if (hasElevator) {
+//                display.append("Elevator is present at this station and working properly.");
+//            } else{
+//                display.append("No elevator is present at this station.");
+//            }
+//        }
 
     }
 }
