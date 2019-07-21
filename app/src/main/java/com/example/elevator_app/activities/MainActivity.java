ab222bd3619cab67d352619d675f5351174819d0
wrinkleintime <ssiner2@gmail.com>
2019-07-21 14:23:21 -0500

    Add todo

--- app/src/main/java/com/example/elevator_app/activities/MainActivity.java
+++ app/src/main/java/com/example/elevator_app/activities/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
     //TODO: Show trash can when swiping to remove favorite
     //TODO: Keep nickname on add favorites
     //TODO: Not rebuild database every time you open
-    //TODO:
+    //TODO: (SAM) Shared preferences for favorites && why is it so slow???
 
     private StationAlertsViewModel mStationAlertsViewModel;
     private FavoritesViewModel mFavoritesViewModel;
